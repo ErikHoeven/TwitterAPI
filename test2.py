827846f19ac8ebdceffbd22bbac3780a0626ad36
Erik van der Hoeven <Erik.Hoeven@gmail.com>
2015-02-04 19:22:38 +0100

    ehn: ntlk.CLASSTRAINGER

--- test2.py
+++ test2.py
@@ -4,11 +4,12 @@ import json
 from json import JSONEncoder
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from nltk.corpus import words
+from Crypto.Util.RFC1751 import wordlist
      
 #Variable declaratie
 strSearchArg = "_id"
 i = 1
-
 
 #JSON Encoder
 class MongoEncoder(JSONEncoder):
@@ -17,8 +18,6 @@ class MongoEncoder(JSONEncoder):
             return str(obj)
         else:            
             return JSONEncoder.default(obj, **kwargs)
-
-
 #Mongo Settings
 client = MongoClient()
 db = client.Sentiment
@@ -30,11 +29,45 @@ TweetTraining = db.TweetTraining
 lstTweets = []
 
 for tweet in TweetTraining.find():
-     strTekst = json.dumps(tweet["tekst"])
-     strSentiment =  json.dumps(tweet["sentiment"])
-     lstTweets[i] = [ strTekst , strSentiment ]
-     #i = i + 1
+     strTekst = json.dumps(tweet["tekst"]).replace("\"", "")
+     strSentiment =  json.dumps(tweet["sentiment"]).replace("\"", "")
+     lstTweets.append([strTekst,strSentiment])
 
 
-#print lstTweets[2]
+#For each tweet loop throught the words and set the words in a list
+lst2Tweets = []
+for (words, sentiment) in lstTweets:
+     word_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+     lst2Tweets.append((word_filtered, sentiment))
+      
+def get_words_in_tweets(lst2Tweets):
+     all_words = []
+     for (words,sentiment) in lst2Tweets:
+         all_words.extend(words)
+     return all_words
+ 
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    word_features =  wordlist.keys()
+    return word_features
 
+word_features = get_word_features(get_words_in_tweets(lst2Tweets))
+
+
+            
+def extract_features (document):
+     documents_word = set(document) 
+     features = {}
+     for word in word_features:
+         features['contains(%s)' % word] = ( word in documents_word )
+     return features
+ 
+trainings_set = nltk.classify.apply_features(extract_features, lst2Tweets)
+#print trainings_set
+classifier = nltk.NaiveBayesClassifier.train(trainings_set)
+
+print classifier.show_most_informative_features(32)
+
+
+
+#print classifier.show_most_informative_features(32)
